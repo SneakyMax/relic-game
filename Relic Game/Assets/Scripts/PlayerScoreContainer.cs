f2d4b0963115e349650ca2802b19c5ed2193ebaa
tapeman <floyd800@gmail.com>
2016-01-30 15:48:51 -0800

    Changed text based score counter to a symbol based score counter

--- Relic Game/Assets/Scripts/PlayerScoreContainer.cs
+++ Relic Game/Assets/Scripts/PlayerScoreContainer.cs
@@ -13,14 +13,40 @@ namespace Assets.Scripts
 
         public int PlayerNumber;
 
+		private Image relic0;
+
+		private Image relic1;
+
+		private Image relic2;
+
+		public void Awake() {
+			SetupScoreText ();
+			Component[] comps = gameObject.GetComponentsInChildren(typeof(Image));
+			foreach(Component comp in comps) {
+				Image img;
+				if(comp is Image)
+					img = (Image) comp;
+				else
+					continue;
+				if(img.name == "Relic0")
+					relic0 = img;
+				else if(img.name == "Relic1")
+					relic1 = img;
+				else if(img.name == "Relic2")
+					relic2 = img;
+			}
+
+			if (ScoreController == null || PlayersDefinition == null || relic0 == null || relic1 == null || relic2 == null)
+				throw new InvalidOperationException("Incorrectly configured.");
+
+			relic0.enabled = false;
+			relic1.enabled = false;
+			relic2.enabled = false;
+		}
+
         public void Start()
         {
-            if (ScoreController == null || PlayersDefinition == null)
-                throw new InvalidOperationException("Incorrectly configured.");
-
             SetBackgroundColor();
-
-            SetupScoreText();
         }
 
         private void SetBackgroundColor()
@@ -28,21 +54,35 @@ namespace Assets.Scripts
             var playerDefinition = PlayersDefinition.Players.FirstOrDefault(x => x.PlayerNumber == PlayerNumber);
 
             var color = playerDefinition.Color;
-            var transparentColor = new Color(color.r, color.g, color.b, 0.3f);
+            var transparentColor = new Color(color.r, color.g, color.b, 0.3f); 
 
             transform.FindChild("Background").GetComponent<Image>().color = transparentColor;
         }
 
         private void SetupScoreText()
         {
-            var formatter = transform.FindChild("ScoreText").GetComponent<TextFormatter>();
-
-            formatter.Set("sc", ScoreController.GetScore(PlayerNumber));
-
             ScoreController.ScoreChanged += (playerNumber, score) =>
             {
-                if (playerNumber == PlayerNumber)
-                    formatter.Set("sc", score);
+                if (playerNumber == PlayerNumber) {
+					Debug.Log("score changed: "+score);
+					if(score == 0) {
+						relic0.enabled = false;
+						relic1.enabled = false;
+						relic2.enabled = false;
+					} else if(score == 1) {
+						relic0.enabled = true;
+						relic1.enabled = false;
+						relic2.enabled = false;
+					} else if(score == 2) {
+						relic0.enabled = true;
+						relic1.enabled = true;
+						relic2.enabled = false;
+					} else if(score == 3) {
+						relic0.enabled = true;
+						relic1.enabled = true;
+						relic2.enabled = true;
+					}
+				}
             };
         }
     }
