88983cc5c0d993be19c90ed7cfc08e59f35a76ca
Andy Brenneke <sneakymax@gmail.com>
2016-01-31 15:26:42 -0800

    General audio controller

---
+++ Relic Game/Assets/Scripts/GeneralAudioController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Linq;
+
+public class GeneralAudioController : MonoBehaviour
+{
+    public GameObject[] AudioObjects;
+
+    public void PlaySoundInstance(string clipName)
+    {
+        var sourceObject =
+            AudioObjects.Select(x => new {Obj = x, Clip = x.GetComponent<AudioSource>().clip.name})
+                .FirstOrDefault(x => x.Clip == clipName);
+
+        if (sourceObject == null)
+            return;
+
+        Instantiate(sourceObject.Obj, new Vector3(), Quaternion.identity);
+    }
+
+    public static void PlaySound(string clipName)
+    {
+        var controller = GameObject.Find("GeneralAudioController");
+        if (controller == null)
+            return;
+
+        controller.GetComponent<GeneralAudioController>().PlaySoundInstance(clipName);
+    }
+}
