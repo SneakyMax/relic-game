bebd5aaae7dce1ab1d8d2879ff122b92de99a8bf
Andy Brenneke <sneakymax@gmail.com>
2016-04-16 15:08:32 -0700

    Start level editor frameowrk, added VFW editor

---
+++ Relic Game/Assets/Scripts/LevelEditor/PlacementGrid.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Vexe.Runtime.Types;
+
+namespace Assets.Scripts.LevelEditor
+{
+    public class PlacementGrid : BaseBehaviour
+    {
+        [AssignedInUnity]
+        public Transform TopLeft;
+
+        /// <summary>The number of grid units across, in <see cref="GridSize"/> increments.</summary>
+        [AssignedInUnity]
+        public int Width = 1;
+
+        [AssignedInUnity]
+        public int Height = 1;
+
+        [AssignedInUnity]
+        public int GridSize = 1;
+    }
+}
